85fc7fab1edeb4d61c7c935f923746bf776c450f
Mads Slotsbo <mads.slotsbo@gmail.com>
2020-06-08 18:40:35 +0200

    Initial Commit

---
+++ src/WorldGenerator.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WorldGenerator.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/WorldGenerator.UI/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WorldGenerator.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateClicked(object sender, RoutedEventArgs e)
+        {
+            var canvas = this.FindName("canvas") as Canvas;
+
+            canvas?.Children.Add(new Polygon
+            {
+                Points = new PointCollection
+                {
+                    { new Point(10, 10) },
+                    { new Point(20, 10) },
+                    { new Point(20, 20) },
+                    { new Point(10, 20) },
+                    { new Point(10, 15) },
+                },
+                Fill = new SolidColorBrush(Color.FromRgb(1, 1, 1))
+            });
+        }
+    }
+}
